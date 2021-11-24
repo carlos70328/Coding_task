3d1e8318ffcb07dff303fb6c80414541702ff362
carlos70328 <82056905+carlos70328@users.noreply.github.com>
2021-11-24 11:36:49 -0500

    actualizaciones

--- src/App.js
+++ src/App.js
@@ -1,150 +1,147 @@
+import React, { useState } from 'react';
 import Imagen from './components/carga';
-import React, { useState, useEffect } from "react";
-import setData from "./components/localStorage"
-import "./login.css";
-import getData from "./components/getData";
-import Swal from 'sweetalert2'
-import comprobar from './components/comprobacion'
-import { iniciarSesion, tomarid } from './components/login'
+import './login.css';
 import ListarRepo from './components/listarRepos';
+import { loginSucces, signUp } from './helpers/loginSucces';
+import { getRepos } from './helpers/getRepos';
 
-let obj = [];
-let bol = true
-let b = []
-let a = 0
-let url = ""
+let idUser = 0;
 
 function App() {
+	const [homeSection, setHomeSection] = useState(true);
+	const [userSection, setUserSection] = useState(false);
+	const [user, setUser] = useState('');
+	const [password, setPassword] = useState('');
+	const [repositorios, setRepositorios] = useState([]);
 
-  const [condicion, setCondicion] = useState(true)
-  const [condicion2, setCondicion2] = useState(false)
-  const [nombre, setNombre] = useState('')
-  const [contrasena, setContrasena] = useState('')
-  const [repositorios, setRepositorios] = useState([])
+	const login = (e) => {
+		e.preventDefault();
+		getRepos(user)
+			.then((repos) => {setRepositorios(repos);})
+			.catch(error => console.log(error));
+		const visibilidad = loginSucces(user, password);
+		setUserSection(visibilidad.user);
+		setHomeSection(visibilidad.home);	
+		idUser = visibilidad.idUser;
+	};
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetch(url)
-        .then(response => response.json())
-        .then(json => setRepositorios(json))
-        .catch(error => console.log(error))
-    }
-    fetchData()
-  }, [b])
+	const singUp = (e) => {
+		e.preventDefault();
+		signUp(user, password);
+	};
 
-  const HandleSubmit2 = (e) => {
-    e.preventDefault()
-    bol = iniciarSesion(nombre, contrasena)
-    if (bol === false) {
-      Swal.fire({
-        icon: 'success',
-        title: "Bienvenido",
-      })
-      setCondicion(bol)
-      a = tomarid()
-      let nombres = getData()
-      if (localStorage.getItem('usuarios') === null) {
-        console.log("sin datos")
-        b = "example"
-      } else {
-        b = (nombres[a].nom)
-      }
-      url = `https://api.github.com/users/${b}/repos`
-      setCondicion2(true)
-    } else {
-      Swal.fire(
-        'Datos incorrectos',
-        'intente nuevamente',
-        'question'
-      )
-    }
-    const formulario = document.querySelector('#fnewUsuario')
-    formulario.reset();
+	const cerrarSesion = (e) => {
+		e.preventDefault();
+		setHomeSection(true);
+		setUserSection(false);
+		localStorage.setItem('repositorios', []);
+	};
 
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    let newUsuario = {
-      id: Date.now(),
-      nom: nombre,
-      cont: contrasena
-    };
-    bol = comprobar(nombre, contrasena)
-    if (bol === true) {
-      obj.push(newUsuario)
-      setData(obj)
-      Swal.fire({
-        icon: 'success',
-        title: "Te has registrado",
-      })
-    } else {
-      Swal.fire('Ya se encuentra registrado')
-    }
-    const formulario = document.querySelector('#fnewUsuario')
-    formulario.reset();
-  }
-
-  const cerrarSesion = (e) => {
-    e.preventDefault()
-    setCondicion(true)
-    setCondicion2(false)
-    localStorage.setItem('repositorios',[ ])
-  }
-
-  return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a href="./" className="navbar-brand">Test<b>Coding</b></a>
-        <div className="navbar-nav ml-auto action-buttons">
-          {condicion &&
-            <div className="nav-item dropdown">
-              <a href="./" data-toggle="dropdown" className="nav-link dropdown-toggle mr-4">Login</a>
-              <div className="dropdown-menu action-form">
-
-                <form id="usuario" onSubmit={HandleSubmit2} >
-                  <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Username" required="required" onChange={e => setNombre(e.target.value)} />
-                  </div>
-                  <div className="form-group">
-                    <input type="password" className="form-control" placeholder="Password" required="required" onChange={e => setContrasena(e.target.value)} />
-                  </div>
-                  <input type="submit" className="btn btn-primary btn-block" value="Login" />
-                </form>
-              </div>
-            </div>}
-          {condicion &&
-            <div className="nav-item dropdown">
-              <a href="./" data-toggle="dropdown" className="btn btn-primary dropdown-toggle sign-up-btn">Sign up</a>
-              <div className="dropdown-menu action-form">
-                <form id="fnewUsuario" onSubmit={handleSubmit} >
-                  <p className="hint-text">Crea una cuenta usanto tu usuario de GitHub</p>
-                  <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Username" required="required" onChange={e => setNombre(e.target.value)} />
-                  </div>
-                  <div className="form-group">
-                    <input type="password" className="form-control" placeholder="Password" required="required" onChange={e => setContrasena(e.target.value)} />
-                  </div>
-                  <input type="submit" className="btn btn-primary btn-block" value="Sign up" />
-                </form>
-              </div>
-            </div>}
-          {condicion2 &&
-            <div className="nav-item dropdown">
-              <div className="dropdown-menu1">
-                <form id="fnewUsuario" type="submit" onSubmit={cerrarSesion}>
-                  <input type="submit" className="btn btn-primary btn-block" value="Log out" />
-                </form>
-              </div>
-            </div>}
-        </div>
-      </nav>
-      {condicion &&
-        <Imagen />}
-      {condicion2 &&
-        <ListarRepo repos={repositorios} n={a} />}
-    </div>
-  );
+	return (
+		<div className='App'>
+			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+				<a href='./' className='navbar-brand'>
+					Test<b>Coding</b>
+				</a>
+				<div className='navbar-nav ml-auto action-buttons'>
+					{homeSection && (
+						<div className='nav-item dropdown'>
+							<a
+								href='./'
+								data-toggle='dropdown'
+								className='nav-link dropdown-toggle mr-4'
+							>
+								Login
+							</a>
+							<div className='dropdown-menu action-form'>
+								<form id='usuario' onSubmit={login}>
+									<div className='form-group'>
+										<input
+											type='text'
+											className='form-control'
+											placeholder='Username'
+											required='required'
+											onChange={(e) => setUser(e.target.value)}
+										/>
+									</div>
+									<div className='form-group'>
+										<input
+											type='password'
+											className='form-control'
+											placeholder='Password'
+											required='required'
+											onChange={(e) => setPassword(e.target.value)}
+										/>
+									</div>
+									<input
+										type='submit'
+										className='btn btn-primary btn-block'
+										value='Login'
+									/>
+								</form>
+							</div>
+						</div>
+					)}
+					{homeSection && (
+						<div className='nav-item dropdown'>
+							<a
+								href='./'
+								data-toggle='dropdown'
+								className='btn btn-primary dropdown-toggle sign-up-btn'
+							>
+								Sign up
+							</a>
+							<div className='dropdown-menu action-form'>
+								<form id='fnewUsuario' onSubmit={singUp}>
+									<p className='hint-text'>
+										Crea una cuenta usanto tu usuario de GitHub
+									</p>
+									<div className='form-group'>
+										<input
+											type='text'
+											className='form-control'
+											placeholder='Username'
+											required='required'
+											onChange={(e) => setUser(e.target.value)}
+										/>
+									</div>
+									<div className='form-group'>
+										<input
+											type='password'
+											className='form-control'
+											placeholder='Password'
+											required='required'
+											onChange={(e) => setPassword(e.target.value)}
+										/>
+									</div>
+									<input
+										type='submit'
+										className='btn btn-primary btn-block'
+										value='Sign up'
+									/>
+								</form>
+							</div>
+						</div>
+					)}
+					{userSection && (
+						<div className='nav-item dropdown'>
+							<div className='dropdown-menu1'>
+								<form id='fnewUsuario' type='submit' onSubmit={cerrarSesion}>
+									<input
+										type='submit'
+										className='btn btn-primary btn-block'
+										value='Log out'
+									/>
+								</form>
+							</div>
+						</div>
+					)}
+				</div>
+			</nav>
+			{homeSection && <Imagen />}
+			{userSection && <ListarRepo repos={repositorios} idUser={idUser} />}
+		</div>
+	);
 }
 
 export default App;
